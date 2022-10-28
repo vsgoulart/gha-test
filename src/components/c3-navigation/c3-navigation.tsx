08d3fdf7a22e753fde86ad60671538b5e1962381
Tim Schuppener <tim.schuppener@camunda.com>
2022-10-28 13:12:58 +0200

    Navbar: sidebar: add nowrap to element buttons

--- src/components/c3-navigation/c3-navigation.tsx
+++ src/components/c3-navigation/c3-navigation.tsx
@@ -373,8 +373,8 @@ const C3NavigationSideBar = (props: {
 									key={element.key}
 									style={
 										index === 0 && !sideBar.customElements
-											? { marginTop: "1.5rem" }
-											: undefined
+											? { marginTop: "1.5rem", whiteSpace: "nowrap" }
+											: { whiteSpace: "nowrap" }
 									}
 									size="sm"
 									kind={element.kind ?? "ghost"}
